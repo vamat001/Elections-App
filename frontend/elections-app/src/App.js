441feda0602c0ebe3d70f5a77a9a146ee5022013
Vivek Amatya <vamat001@ucr.edu>
2019-10-22 00:58:17 -0700

    basic hello world in react

---
+++ frontend/elections-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>Hello World!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
