37ca6581cb1a1c71d49265fa934ef7375b9cb4b4
Vivek Amatya <vamat001@ucr.edu>
2019-10-23 11:45:05 -0700

    added components

--- frontend/elections-app/src/App.js
+++ frontend/elections-app/src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
-import LandingPage from "./landingPage.js";
+import LandingPage from "./components/landingPage.js";
 import "./App.css";
 
 class App extends Component {
-   render(){
-      return(
-         <LandingPage/>
-      );
-   }
-};
+  render() {
+    return <LandingPage />;
+  }
+}
 
 export default App;
