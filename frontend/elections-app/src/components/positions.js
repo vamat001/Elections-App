4f5d791cff5f38d664fba0e867af416d5b6402b4
Vivek Amatya <vamat001@ucr.edu>
2019-11-20 01:42:09 -0800

    referendums pulled from firebase

--- frontend/elections-app/src/components/positions.js
+++ frontend/elections-app/src/components/positions.js
@@ -1,84 +1,95 @@
-import React, { Component, listgroup, listgroupitem, list, CardGroup, media } from "react";
+import React, {
+  Component,
+  listgroup,
+  listgroupitem,
+  list,
+  CardGroup,
+  media
+} from "react";
 import firebase, { auth, provider, firestore } from "./firebase.js";
-import { Card, CardDeck } from 'react-bootstrap';
+import { Card, CardDeck } from "react-bootstrap";
 
 class positions extends Component {
-    constructor() {
-        super();
-        firebase.firestore().collection('Positions')
-            .onSnapshot(
-            querySnapshot => {this.setState({
+  constructor() {
+    super();
+    firebase
+      .firestore()
+      .collection("Positions")
+      .onSnapshot(
+        querySnapshot => {
+          this.setState({
             positionsArray: querySnapshot.docs.map(doc => {
-                      return { undergradCands: doc.data().undergradCandidates,
-                      description: doc.data().description, 
-                      id: doc.id };
-                    }),
-                });
-            },
-            () => null,
-        );
-       firebase.firestore().collection('Candidates')
-            .onSnapshot(
-            querySnapshot => {this.setState({
+              return {
+                undergradCands: doc.data().undergradCandidates,
+                description: doc.data().description,
+                id: doc.id
+              };
+            })
+          });
+        },
+        () => null
+      );
+    firebase
+      .firestore()
+      .collection("Candidates")
+      .onSnapshot(
+        querySnapshot => {
+          this.setState({
             candidatesArray: querySnapshot.docs.map(doc => {
-                      return { runningFor: doc.data().runningFor,
-                      name: doc.data().name, 
-                      id: doc.data().ID,
-                      gradStudent: doc.data().gradStudent,
-                      description: doc.data().description,
-                      major: doc.data().major };
-                    }),
-                });
-            },
-            () => null,
-        );
-    }
+              return {
+                runningFor: doc.data().runningFor,
+                name: doc.data().name,
+                id: doc.data().ID,
+                gradStudent: doc.data().gradStudent,
+                description: doc.data().description,
+                major: doc.data().major
+              };
+            })
+          });
+        },
+        () => null
+      );
+  }
 
   render() {
-    if(this.state){
-        const positionNames = this.state.positionsArray.map(pos => {
-            if(this.state.candidatesArray){
-                const cardLists = this.state.candidatesArray.map(can =>{
-                    if(can.runningFor === pos.id && !can.gradStudent){
-                        return  <CardDeck >
-                        <div className='mt-1'>
-                        <Card border="dark" bg="light" >
-                                  <Card.Body>
-                                    <Card.Title>{can.name}</Card.Title>
-                                    <Card.Subtitle className="mb-2 text-muted">{can.major}</Card.Subtitle>
-                                    <Card.Text>
-                                        {can.description}
-                                    </Card.Text>
-                                  </Card.Body>
-                                </Card>
-                        </div>
-                                </CardDeck>;
-                    }
-                });
-
-              return <li  class="list-group-item"> 
-                    <h1 class="list-group-item-heading">{pos.id}</h1>
-                    <p class="list-group-item-text">{pos.description}</p>
-                    <Card bg="info" text="black" style={{ width: 'auto' }}>
-                        <Card.Header>Candidates</Card.Header>
-                        <Card.Body>
-                                {cardLists}
-                        </Card.Body>
-                      </Card>
-                </li>;
-
+    if (this.state) {
+      const positionNames = this.state.positionsArray.map(pos => {
+        if (this.state.candidatesArray) {
+          const cardLists = this.state.candidatesArray.map(can => {
+            if (can.runningFor === pos.id && !can.gradStudent) {
+              return (
+                <CardDeck>
+                  <div className="mt-1">
+                    <Card border="dark" bg="light">
+                      <Card.Body>
+                        <Card.Title>{can.name}</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">
+                          {can.major}
+                        </Card.Subtitle>
+                        <Card.Text>{can.description}</Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </CardDeck>
+              );
             }
-        });
-        return (
-            <div class="list-group">
-                {positionNames}
-            </div>
-        );
-    }else{
-        return (
-        <div class="list-group">
-        </div>
-        );
+          });
+
+          return (
+            <li class="list-group-item">
+              <h1 class="list-group-item-heading">{pos.id}</h1>
+              <p class="list-group-item-text">{pos.description}</p>
+              <Card bg="info" text="black" style={{ width: "auto" }}>
+                <Card.Header>Candidates</Card.Header>
+                <Card.Body>{cardLists}</Card.Body>
+              </Card>
+            </li>
+          );
+        }
+      });
+      return <div class="list-group">{positionNames}</div>;
+    } else {
+      return <div class="list-group"></div>;
     }
   }
 }
--- frontend/elections-app/src/components/referendums.js
+++ frontend/elections-app/src/components/referendums.js
@@ -1,56 +1,86 @@
 import React, { Component } from "react";
 import NavHeader from "./NavHeader";
-import axios from "axios";
+import firebase from "./firebase.js";
 
 const API_BASE = "http://localhost:5000/elections-app-4e4df/us-central1/api";
 
 class referendums extends Component {
   constructor() {
     super();
-    this.state = { Referendums: [] };
+    firebase
+      .firestore()
+      .collection("Referendums")
+      .onSnapshot(
+        querySnapshot => {
+          this.setState({
+            referendumsArray: querySnapshot.docs.map(doc => {
+              return {
+                implications: doc.data().implications,
+                description: doc.data().description
+              };
+            })
+          });
+        },
+        () => null
+      );
   }
-  getReferendums = async () => {
-    await axios.get(API_BASE + "/getSecretary").then(res => {
-      this.setState({ secretary: res.data });
-    });
-  };
   render() {
-    return (
-      <body
-        data-spy="scroll"
-        data-target=".site-navbar-target"
-        data-offset="300"
-      >
-        <div class="site-wrap">
-          <div class="site-mobile-menu site-navbar-target">
-            <div class="site-mobile-menu-header">
-              <div class="site-mobile-menu-close mt-3">
-                <span class="icon-close2 js-menu-toggle"></span>
-              </div>
-            </div>
-            <div class="site-mobile-menu-body"></div>
-          </div>
-
-          <div class="container d-none d-lg-block">
-            <div class="row">
-              <div class="col-12 text-center mb-4 mt-5">
-                <h1 class="mb-0 site-logo text-black h2 mb-0">
-                  UCR Elections<span class="text-primary"></span>
-                </h1>
-              </div>
-            </div>
-          </div>
-          <NavHeader />
-          {/*Referendums*/}
-          <div class="site-section" id="Referendums">
-            <div class="container">
-              <div class="row mb-5">
-                <div class="col-12 text-center">
-                  <h2 class="section-title mb-3">Referendums</h2>
+    if (this.state) {
+      if (this.state.referendumsArray) {
+        const cards = this.state.referendumsArray.map(ref => {
+          return (
+            <div class="col-md-6 col-lg-4 mb-4 mb-lg-4" data-aos="fade-up">
+              <div class="unit-4 d-block">
+                <div class="unit-4-icon mb-3">
+                  <span class="icon-wrap">
+                    <span class="text-primary icon-autorenew"></span>
+                  </span>
+                </div>
+                <div>
+                  <h3>Referendum</h3>
+                  <p>{ref.description}</p>
                 </div>
               </div>
-              <div class="row align-items-stretch">
-                <div class="col-md-6 col-lg-4 mb-4 mb-lg-4" data-aos="fade-up">
+            </div>
+          );
+        });
+        return (
+          <body
+            data-spy="scroll"
+            data-target=".site-navbar-target"
+            data-offset="300"
+          >
+            <div class="site-wrap">
+              <div class="site-mobile-menu site-navbar-target">
+                <div class="site-mobile-menu-header">
+                  <div class="site-mobile-menu-close mt-3">
+                    <span class="icon-close2 js-menu-toggle"></span>
+                  </div>
+                </div>
+                <div class="site-mobile-menu-body"></div>
+              </div>
+
+              <div class="container d-none d-lg-block">
+                <div class="row">
+                  <div class="col-12 text-center mb-4 mt-5">
+                    <h1 class="mb-0 site-logo text-black h2 mb-0">
+                      UCR Elections<span class="text-primary"></span>
+                    </h1>
+                  </div>
+                </div>
+              </div>
+              <NavHeader />
+              {/*Referendums*/}
+              <div class="site-section" id="Referendums">
+                <div class="container">
+                  <div class="row mb-5">
+                    <div class="col-12 text-center">
+                      <h2 class="section-title mb-3">Referendums</h2>
+                    </div>
+                  </div>
+                  <div class="row align-items-stretch">
+                    {cards}
+                    {/*} <div class="col-md-6 col-lg-4 mb-4 mb-lg-4" data-aos="fade-up">
                   <div class="unit-4 d-block">
                     <div class="unit-4-icon mb-3">
                       <span class="icon-wrap">
@@ -148,20 +178,24 @@ class referendums extends Component {
                       </p>
                     </div>
                   </div>
-                </div>
-                <div>
-                  <p style={{ marginLeft: 1 + "em" }}>
-                    <a href="#" class="btn btn-primary">
-                      More Info
-                    </a>
-                  </p>
+    </div> */}
+                    <div>
+                      <p style={{ marginLeft: 1 + "em" }}>
+                        <a href="#" class="btn btn-primary">
+                          More Info
+                        </a>
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      </body>
-    );
+          </body>
+        );
+      }
+    } else {
+      return <div></div>;
+    }
   }
 }
 
