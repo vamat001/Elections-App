2df66fdbb4335d03880f35e72039d61e394dabc2
TimothyKool <tkoo005@ucr.edu>
2019-11-30 22:47:55 -0800

    Candidates sidebar and base cards

---
+++ frontend/elections-app/src/components/Sidebar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Sidebar.css";
+
+class sidebar extends React.Component {
+  render() {
+    return (
+      // <div class="sidebarOutter">
+      //   <div class="sidebarInner">
+      //     <ul>
+      //       <li style={{cursor: 'pointer'}} onClick={() =>this.select("President")}> 
+      //         Item 1
+      //       </li>
+      //       <hr/>
+      //       <li>
+      //         Item 2
+      //       </li>
+      //       <hr/>
+      //       <li>
+      //         Item 2
+      //       </li>
+      //     </ul>
+      //   </div>
+      // </div>
+          <div class="col-sm-12 col-md-6 col-lg-3">
+            <ul class="list-group">
+              <li class="list-group-item tableItem">President</li>
+              <li class="list-group-item tableItem">Executive Vice President</li>
+              <li class="list-group-item tableItem">Vice President of Internal Affairs</li>
+              <li class="list-group-item tableItem">Vice President of External Affairs</li>
+              <li class="list-group-item tableItem">Vice President of Finance</li>
+              <li class="list-group-item tableItem">Undergraduate Sustainability Director</li>
+              <li class="list-group-item tableItem">Personnel Director</li>
+              <li class="list-group-item tableItem">Marketing and Promotions Director</li>
+              <li class="list-group-item tableItem">Transfer/Non-Traditional Student Director</li>
+            </ul>
+          </div>
+        
+    );
+  }
+}
+
+export default sidebar;
--- frontend/elections-app/src/components/candidates.js
+++ frontend/elections-app/src/components/candidates.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import NavHeader from "./NavHeader.js";
+import Sidebar from "./Sidebar.js"
+import "./Sidebar.css"
 import { Card, CardDeck } from "react-bootstrap";
 import firebase, { auth, provider, firestore } from "./firebase.js";
 
@@ -26,41 +29,87 @@ class candidates extends Component {
         () => null
       );
   }
-  render() {
-    if (this.state) {
-      if (this.state.candidatesArray) {
-        const cardLists = this.state.candidatesArray.map(can => {
-          return (
-            <CardDeck>
-              <div className="mt-1">
-                <Card border="dark" bg="light" style={{ width: "auto" }}>
-                  <Card.Body>
-                    <Card.Title>{can.name}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">
-                      Running For: {can.runningFor}
-                    </Card.Subtitle>
-                    <Card.Text>
-                      I'm majoring in {can.major}. {can.description}
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </div>
-            </CardDeck>
-          );
-        });
 
-        return (
-          <li class="list-group-item">
-            <Card bg="info" text="black" style={{ width: "auto" }}>
-              <Card.Header>All Candidates</Card.Header>
-              <Card.Body>{cardLists}</Card.Body>
-            </Card>
-          </li>
-        );
-      }
-    } else {
-      return <div></div>;
-    }
+select = (pos) => {
+  this.state.showPos = pos;
+}
+
+  render() {
+    return(
+      <div>
+        <NavHeader/>
+        <div class="container site-section padTop">
+          <div class="row">
+            <Sidebar/>
+
+            <div class="card-deck col-lg-9">
+              <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap"></img>
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+              <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap"></img>
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+              <div class="card">
+                <img class="card-img-top" src="..." alt="Card image cap"></img>
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        {/* <Sidebar/> */}
+
+      </div>
+      
+    );
+    // if (this.state) {
+    //   if (this.state.candidatesArray) {
+    //     const cardLists = this.state.candidatesArray.map(can => {
+    //       return (
+    //         <CardDeck>
+    //           <div className="mt-1">
+    //             <Card border="dark" bg="light" style={{ width: "auto" }}>
+    //               <Card.Body>
+    //                 <Card.Title>{can.name}</Card.Title>
+    //                 <Card.Subtitle className="mb-2 text-muted">
+    //                   Running For: {can.runningFor}
+    //                 </Card.Subtitle>
+    //                 <Card.Text>
+    //                   I'm majoring in {can.major}. {can.description}
+    //                 </Card.Text>
+    //               </Card.Body>
+    //             </Card>
+    //           </div>
+    //         </CardDeck>
+    //       );
+    //     });
+
+    //     return (
+    //       <li class="list-group-item">
+    //         <Card bg="info" text="black" style={{ width: "auto" }}>
+    //           <Card.Header>All Candidates</Card.Header>
+    //           <Card.Body>{cardLists}</Card.Body>
+    //         </Card>
+    //       </li>
+    //     );
+    //   }
+    // } else {
+    //   return <div></div>;
+    // }
   }
 }
 
